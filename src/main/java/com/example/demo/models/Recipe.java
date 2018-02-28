29809ce287aeda0bd75787b6979e36b6a1502a2d
michael.menard <mlmmayhem@gmail.com>
2018-02-28 12:48:05 -0700

    created an enumeration type for difficulty rating of a recipe

--- src/main/java/com/example/demo/models/Recipe.java
+++ src/main/java/com/example/demo/models/Recipe.java
@@ -18,7 +18,8 @@ public class Recipe {
     private String url;
     private String directions;
 
-//    private Difficulty difficulty;
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -107,5 +108,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }

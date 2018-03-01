03d4f45ed175cac16585e1dd235e417362875493
michael.menard <mlmmayhem@gmail.com>
2018-03-01 16:42:33 -0700

    implemented a test for showing Recipe by id
    wrote the test, then implemented methods

---
+++ src/test/java/com/example/demo/controllers/RecipeControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Recipe;
+import com.example.demo.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void getRecipeTest() throws Exception {
+
+        /* given */
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        /* when */
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        /* then */
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}

40362beb5328675cd1683f5c43472fbd01c975ad
michael.menard <mlmmayhem@gmail.com>
2018-03-01 12:20:15 -0700

    wrote an Entity unit test

---
+++ src/test/java/com/example/demo/controllers/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getIndex() {
+        /* given */
+        /* when */
+        /* then */
+    }
+}

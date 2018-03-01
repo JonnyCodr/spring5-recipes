e0214266f07d4aba790df81bf9913ab65f239a7b
michael.menard <mlmmayhem@gmail.com>
2018-03-01 12:28:38 -0700

    controller test using mocks

--- src/test/java/com/example/demo/controllers/IndexControllerTest.java
+++ src/test/java/com/example/demo/controllers/IndexControllerTest.java
@@ -4,9 +4,18 @@ import com.example.demo.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import static org.junit.Assert.*;
+
+
 
 public class IndexControllerTest {
 
@@ -16,14 +25,26 @@ public class IndexControllerTest {
     @Mock
     Model model;
 
+    IndexController indexController;
+
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
     }
 
     @Test
     public void getIndex() {
         /* given */
+
+
         /* when */
+        String viewName = indexController.getIndex(model);
+
         /* then */
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
     }
 }

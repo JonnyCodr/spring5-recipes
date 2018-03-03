1b949eceead0fc1c4521851336cc321cda679261
michaelmenard <mlmmayhem@gmail.com>
2018-03-03 16:30:57 -0700

    handling the upload and persisting of an image to the db

--- src/main/java/com/example/demo/services/ImageServiceImpl.java
+++ src/main/java/com/example/demo/services/ImageServiceImpl.java
@@ -1,12 +1,52 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Recipe;
+import com.example.demo.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+
+/**
+ * Created by jt on 7/3/17.
+ */
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
-    @Override
-    public void saveImageFile(Long recipeId, MultipartFile imageFile) {
 
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+//            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }

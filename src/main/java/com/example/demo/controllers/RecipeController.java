5e3fde34bf6ce771b2f29621b14c71cf4c43820b
michaelmenard <mlmmayhem@gmail.com>
2018-03-03 17:43:32 -0700

    add id value to the 404 error view

--- src/main/java/com/example/demo/controllers/RecipeController.java
+++ src/main/java/com/example/demo/controllers/RecipeController.java
@@ -55,10 +55,12 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
-//        log.error("handleing not found exception");
+    public ModelAndView handleNotFound(Exception exception) {
+//        log.error("handling not found exception");
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
         return modelAndView;
     }
 
